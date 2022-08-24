29e2a21b65062612d5c0e69d34f18e8065fd845e
Lucas Reis <lucasreis64@gmail.com>
2022-08-23 23:45:14 -0300

    Login e online

---
+++ assets/script/script.js
@@ -0,0 +1,39 @@
+const nomeurl = 'https://mock-api.driven.com.br/api/v6/uol/participants';
+const onlineurl = 'https://mock-api.driven.com.br/api/v6/uol/status'
+const login = document.querySelector('.conteudo-login');
+let promessa, usuario;
+function pegarNome(){
+    userName();
+    promessa.then(chat);
+    promessa.catch(deuErro);
+}
+
+function deuErro() {
+    alert('Nome já está em uso, tente novamente!');
+}
+
+function chat(){
+    setInterval(online, 5000);
+    login.classList.add('escondido');
+    
+}
+
+function userName(){
+    let nome = document.querySelector('.nome');
+    nome = nome.value;
+    console.log(nome);
+    usuario = {
+        name: nome
+    };
+    console.log(usuario);
+    promessa = axios.post(nomeurl, usuario);
+}
+
+function online () {
+    const username = axios.post(onlineurl, usuario);
+    username.then(situ);
+}
+
+function situ () {
+    console.log('Status: online');
+}
