5c7d4a1817360f1a654f6f5af9bf00625f6ac32f
Lucas Reis <lucasreis64@gmail.com>
2022-08-25 17:29:06 -0300

    Fazendo bônus mensagem privada

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,13 +4,27 @@ const mensagemurl = 'https://mock-api.driven.com.br/api/v6/uol/messages';
 const login = document.querySelector('.conteudo-login');
 const papo = document.querySelector('.chat');
 const enviada = document.querySelector('.enviada');
-const todos = document.querySelector('.usuarios>div')
-let promessa, usuario,entry=0;;
+const todos = document.querySelector('.usuarios>div');
+const bottom = document.querySelector('.bottombar');
+const inferior = document.querySelector('.login');
+const lateral = document.querySelector('.barralateral');
+const usuarios = document.querySelector('.usuarios');
+const tipoenvio = document.querySelector('.tipoEnvio');
+let selecionado;
+
+let envio = {
+    to: "Todos",
+    type: "message",
+    from: '',
+    text:''
+};
+let promessa, usuario, entry = 0;
+let msganterior = [];
 
 document.addEventListener('keydown', ola);
 
 function ola(event) {
-    if (event.keyCode === 13){
+    if (event.keyCode === 13) {
         pegarNome();
     }
 }
@@ -29,17 +43,17 @@ function deuErro() {
 
 
 
-function carregamento(){
-    const inferior = document.querySelector('.login');
+function carregamento() {
     deletar(inferior);
     const carregar = document.createElement('img');
     const textoinferior = document.createElement('div');
-    carregar.src='./assets/imagens/carregando.gif';
-    textoinferior.innerHTML='Entrando...';
+    carregar.src = './assets/imagens/carregando.gif';
+    textoinferior.innerHTML = 'Entrando...';
     inferior.appendChild(carregar);
     inferior.appendChild(textoinferior);
     setTimeout(chat, 3000);
     setInterval(carregarMensagens, 3000);
+    //carregarMensagens();
 }
 
 function chat() {
@@ -49,10 +63,10 @@ function chat() {
 
     login.classList.add('escondido');
     document.body.classList.add('fundo');
-    
+
 }
 
-function mostrar(item){
+function mostrar(item) {
     item.classList.remove('escondido');
 }
 
@@ -88,22 +102,22 @@ function retorno(mensagem) {
         const msg = document.createElement('div');
         msg.classList.add('mensagem');
         if (mensagem[cont].type === 'message') {
-            msg.innerHTML = `<span class = "horacinza">(${mensagem[cont].time})</span> <span>${mensagem[cont].from}</span> para <span class="margin0">${mensagem[cont].to}</span>: ${mensagem[cont].text}`;
+            msg.innerHTML = `<strong class = "horacinza">(${mensagem[cont].time})</strong> <strong>${mensagem[cont].from}</strong> para <strong class="margin0">${mensagem[cont].to}</strong>: ${mensagem[cont].text}`;
         }
         if (mensagem[cont].type === 'status') {
             msg.classList.add('cinza');
-            msg.innerHTML = `<span class = "horacinza">(${mensagem[cont].time})</span> <span>${mensagem[cont].from}</span> ${mensagem[cont].text}`;
+            msg.innerHTML = `<strong class = "horacinza">(${mensagem[cont].time})</strong> <strong>${mensagem[cont].from}</strong> ${mensagem[cont].text}`;
         }
         if (mensagem[cont].type === 'private_message') {
             msg.classList.add('rosa');
-            msg.innerHTML = `<span class = "horacinza">(${mensagem[cont].time})</span> <span>${mensagem[cont].from}</span> reservadamente para <span class="margin0">${mensagem[cont].to}</span>: ${mensagem[cont].text}`;
+            msg.innerHTML = `<strong class = "horacinza">(${mensagem[cont].time})</strong> <strong>${mensagem[cont].from}</strong> reservadamente para <strong class="margin0">${mensagem[cont].to}</strong>: ${mensagem[cont].text}`;
         }
         papo.appendChild(msg);
-        if (cont===mensagem.length-1 && entry===0){
+        if ((cont === mensagem.length - 1 && msganterior !== mensagem.data)) {
             msg.scrollIntoView();
         }
     }
-    entry++;
+    msganterior = mensagem;
 }
 
 function deletar(papo) {
@@ -112,43 +126,54 @@ function deletar(papo) {
     }
 }
 
-function enviarMensagem(){
-    
-    const envio = {
-        from: usuario.name,
-        to: "Todos",
-        text: enviada.value,
-        type: "message"
-    };
+function enviarMensagem() {
+    selecionado = document.querySelector(('.selecionado'));
+    envio.from= usuario.name;
+    envio.text= enviada.value;
+    if (selecionado !== null) {
+        if (selecionado.classList.contains('publico') === true) {}
+        if (selecionado.classList.contains('reservado') === true) {
+            envio.type = 'private_message';
+        }
+    }
     console.log(envio);
-    const teste = envio.text.trim(); 
-    const enviar = axios.post(mensagemurl,envio);
+    const teste = envio.text.trim();
+    const enviar = axios.post(mensagemurl, envio);
     enviar.then(carregarMensagens);
-    if (teste!==''){enviar.catch(atualizar);}
-    else {enviar.catch(alertar);}
-    enviada.value='';
+    if (teste !== '') {
+        enviar.catch(atualizar);
+    } else {
+        enviar.catch(alertar);
+    }
+    enviada.value = '';
 }
-function atualizar(){
+
+function atualizar() {
     alert('Você não está mais na sala! Aperte "ok" para entrar novamente.');
     window.location.reload();
 }
 
-function alertar(){
+function envioReservado() {
+
+
+}
+
+function alertar() {
     alert('Mensagem em branco! Tente novamente!');
 }
 
 enviada.addEventListener('keydown', enter);
 
 function enter(event) {
-    if (event.keyCode === 13){
+    if (event.keyCode === 13) {
         enviarMensagem();
     }
 }
 
-function mostrarLateral(){
+function mostrarLateral() {
     usuariosOnline();
     setInterval(usuariosOnline, 5000);
-    const lateral = document.querySelector('.barralateral');
+
     lateral.classList.remove('oculto');
 }
 
@@ -157,26 +182,51 @@ function usuariosOnline() {
     users.then(mostrarParticipantes);
     users.catch(deuErro);
 }
-
 function mostrarParticipantes(part) {
     part = part.data;
     console.log(part);
-    const usuarios=document.querySelector('.usuarios');
+
     deletar(usuarios);
     usuarios.appendChild(todos);
-    for (let cont=0;  cont<part.length; cont++){
-    const divi = document.createElement('div');
-    const icon = document.createElement('ion-icon');
-    const divitexto = document.createElement('div');
-    divitexto.innerHTML = part[cont].name;
-    icon.setAttribute('name', 'person-circle')
-    usuarios.appendChild(divi);
-    divi.appendChild(icon);
-    divi.appendChild(divitexto);
+    for (let cont = 0; cont < part.length; cont++) {
+        const divi = document.createElement('div');
+        const icon = document.createElement('ion-icon');
+        const divitexto = document.createElement('div');
+        divitexto.innerHTML = part[cont].name;
+        icon.setAttribute('name', 'person-circle');
+        usuarios.appendChild(divi);
+        divi.appendChild(icon);
+        divi.appendChild(divitexto);
+        divi.id=part[cont].name;
+        divi.addEventListener('click',selecionarUsuario);
     }
 }
 
-function fecharLateral(){
-    const lateral = document.querySelector('.barralateral');
+function selecionarUsuario(){
+    this.style.backgroundColor = 'blue';
+}
+
+
+function fecharLateral() {
     lateral.classList.add('oculto');
+}
+
+function selecionarVisibilidade(esse) {
+    selecionado = document.querySelectorAll(('.selecionado'));
+    selecionado.forEach(removerCheck);
+    esse.classList.add('selecionado');
+    const icon = document.createElement('ion-icon');
+    icon.setAttribute('name', 'checkmark');
+    icon.classList.add('verde', 'check');
+    esse.appendChild(icon);
+    if (esse.classList.contains('reservado') === true) {
+        tipoenvio.innerHTML = `Enviando para ${envio.to} (reservadamente)`;
+    }
+}
+
+function removerCheck(este) {
+    const check = document.querySelector('.check');
+    console.log(este);
+    este.classList.remove('selecionado');
+    este.removeChild(check);
 }
